eebd9605a28dd014766542cf48b8b3b77344b270
Vinicius Brisotti <vinicius.brisotti@hotmail.com>
2023-12-07 20:21:55 -0300

    Angular good  practices applied

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsObraComponent } from './components/forms-obra/forms-obra.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { FormsObraNovoComponent } from './components/forms-obra-novo/forms-obra-novo.component';
 
 @NgModule({
