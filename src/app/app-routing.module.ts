d96f963eb76a53202ebeb858e63f092bb3e1ce44
Kaiqoliver <kakadeoliveira@outlook.com>
2023-12-16 22:25:43 -0300

    Adding routs to the forms to make them separable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FormUsuarioComponent } from './components/forms/form-usuario/form-usuario.component';
+import { FormsArquitetoComponent } from './components/forms/forms-arquiteto/forms-arquiteto.component';
+import { FormsConstrutoraComponent} from './components/forms/forms-construtora/forms-construtora.component';
+import { FormsObraComponent} from './components/forms/forms-obra/forms-obra.component';
+
+
+
+const routes: Routes = [
+  { path: 'form-usuario', component: FormUsuarioComponent },
+  { path: 'form-arquiteto', component: FormsArquitetoComponent },
+  { path: 'form-construtora', component: FormsConstrutoraComponent },
+  { path: 'form-obra', component: FormsObraComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
