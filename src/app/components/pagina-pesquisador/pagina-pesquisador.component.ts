1aa52c94bb7d9408bb35b2a13f199c3928e6161c
Kaiqoliver <kakadeoliveira@outlook.com>
2023-12-17 22:00:42 -0300

    Adding social media icons, adding footer, creating empty pesquisador page

---
+++ src/app/components/pagina-pesquisador/pagina-pesquisador.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-pesquisador',
+  templateUrl: './pagina-pesquisador.component.html',
+  styleUrls: ['./pagina-pesquisador.component.css']
+})
+export class PaginaPesquisadorComponent {
+
+}
