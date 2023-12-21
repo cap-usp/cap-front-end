50fb6a679ca54c7f9e1c5e0e1d2ac0088dae5f69
Kaiqoliver <kakadeoliveira@outlook.com>
2023-12-21 17:49:40 -0300

    Creating first services and API calls

---
+++ src/app/components/lists-forms/construtora-list/construtora-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Construtora } from 'src/app/models/construtora.model';
+import { ConstrutoraService } from 'src/app/services/construtora-service/construtora.service';
+
+@Component({
+  selector: 'app-construtora-list',
+  templateUrl: './construtora-list.component.html',
+  styleUrls: ['./construtora-list.component.css']
+})
+export class ConstrutoraListComponent {
+  construtoras : Construtora[] = [];
+
+  constructor (private construtoraService: ConstrutoraService) {
+    this.getConstrutoras();
+  }
+
+  getConstrutoras() {
+    this.construtoraService.getConstrutoras().subscribe(construtoras => this.construtoras = construtoras);
+  }
+}
---
+++ src/app/services/construtora-service/construtora.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Construtora } from 'src/app/models/construtora.model';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConstrutoraService {
+
+  private url = environment.api;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getConstrutoras() {
+    return this.httpClient.get<Construtora[]>(this.url + '/construtora');
+  }
+}
