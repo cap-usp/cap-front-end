da2329801f0f1d4c8dab1da9cb1cef308c6f8f0a
Kaiqoliver <kakadeoliveira@outlook.com>
2024-01-10 00:47:08 -0300

    Adding router links for the other entities

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,16 +7,18 @@ import { FormsConstrutoraComponent} from './components/construtora/forms-constru
 import { FormsObraComponent} from './components/obra/forms-obra/forms-obra.component';
 import { PaginaPesquisadorComponent } from './components/pagina-pesquisador/pagina-pesquisador.component';
 import { FormListConstrutoraComponent } from './components/construtora/form-list-construtora/form-list-construtora.component';
+import { FormListArquitetoComponent } from './components/arquiteto/form-list-arquiteto/form-list-arquiteto.component';
+import { FormListObraComponent } from './components/obra/form-list-obra/form-list-obra.component';
+import { FormListUsuarioComponent } from './components/usuario/form-list-usuario/form-list-usuario.component';
 
 
 
 const routes: Routes = [
-  { path: 'form-usuario', component: FormUsuarioComponent },
-  { path: 'form-arquiteto', component: FormsArquitetoComponent },
-  { path: 'form-construtora', component: FormsConstrutoraComponent },
-  { path: 'form-obra', component: FormsObraComponent },
   { path: 'pagina-pesquisador', component: PaginaPesquisadorComponent},
-  { path: 'form-list-construtora', component: FormListConstrutoraComponent}
+  { path: 'form-list-construtora', component: FormListConstrutoraComponent},
+  { path: 'form-list-arquiteto', component: FormListArquitetoComponent},
+  { path: 'form-list-obra', component: FormListObraComponent},
+  { path: 'form-list-usuario', component: FormListUsuarioComponent}
 ];
 
 @NgModule({
