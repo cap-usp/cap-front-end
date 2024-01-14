b6f46d755e1948f15ae4b24a184ceda669fcf5fd
Kaiqoliver <kakadeoliveira@outlook.com>
2024-01-14 19:51:44 -0300

    Changing styles for the obra components and fixing display errors for the list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { FormUsuarioComponent } from './components/usuario/form-usuario/form-usuario.component';
-import { FormsArquitetoComponent } from './components/arquiteto/forms-arquiteto/forms-arquiteto.component';
-import { FormsConstrutoraComponent} from './components/construtora/forms-construtora/forms-construtora.component';
-import { FormsObraComponent} from './components/obra/forms-obra/forms-obra.component';
+
 import { PaginaPesquisadorComponent } from './components/pagina-pesquisador/pagina-pesquisador.component';
 import { FormListConstrutoraComponent } from './components/construtora/form-list-construtora/form-list-construtora.component';
 import { FormListArquitetoComponent } from './components/arquiteto/form-list-arquiteto/form-list-arquiteto.component';
 import { FormListObraComponent } from './components/obra/form-list-obra/form-list-obra.component';
 import { FormListUsuarioComponent } from './components/usuario/form-list-usuario/form-list-usuario.component';
-
+import { ListObraComponent } from './components/obra/list-obra/list-obra.component';
 
 
 const routes: Routes = [
@@ -18,7 +15,8 @@ const routes: Routes = [
   { path: 'form-list-construtora', component: FormListConstrutoraComponent},
   { path: 'form-list-arquiteto', component: FormListArquitetoComponent},
   { path: 'form-list-obra', component: FormListObraComponent},
-  { path: 'form-list-usuario', component: FormListUsuarioComponent}
+  { path: 'form-list-usuario', component: FormListUsuarioComponent},
+  { path: 'list-obra', component: ListObraComponent}
 ];
 
 @NgModule({
