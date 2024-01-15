f99866df0ce7f490f8c836d4e9bdb30f42d1b0f8
Gabriel Arrais <arrais.gbr@usp.br>
2024-01-15 10:09:40 -0300

    creating pagina-inicial component and add its route to main logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,10 +10,12 @@ import { FormListConstrutoraComponent } from './components/construtora/form-list
 import { FormListArquitetoComponent } from './components/arquiteto/form-list-arquiteto/form-list-arquiteto.component';
 import { FormListObraComponent } from './components/obra/form-list-obra/form-list-obra.component';
 import { FormListUsuarioComponent } from './components/usuario/form-list-usuario/form-list-usuario.component';
+import { PaginaInicialComponent } from './components/pagina-inicial/pagina-inicial.component';
 
 
 
 const routes: Routes = [
+  { path: '', component: PaginaInicialComponent, pathMatch: 'full'},
   { path: 'pagina-pesquisador', component: PaginaPesquisadorComponent},
   { path: 'form-list-construtora', component: FormListConstrutoraComponent},
   { path: 'form-list-arquiteto', component: FormListArquitetoComponent},
