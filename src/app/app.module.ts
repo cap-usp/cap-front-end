195ae64c4e7c4f5db6a8703cd12f94c1da457f08
Gabriel Arrais <arrais.gbr@usp.br>
2024-02-01 23:46:38 -0300

    removing old component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { PaginaPesquisadorComponent } from './components/pagina-pesquisador/pagina-pesquisador.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import { FooterComponent } from './components/footer/footer.component';
@@ -35,7 +34,6 @@ import { LoginComponent } from './components/login/login.component';
     FormsArquitetoComponent,
     FormsConstrutoraComponent,
     FormUsuarioComponent,
-    PaginaPesquisadorComponent,
     FooterComponent,
     ConstrutoraListComponent,
     HeaderComponent,
