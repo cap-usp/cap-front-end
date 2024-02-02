7cebd6783111387db13bdb9ff37b72f95cff0852
Gabriel Arrais <arrais.gbr@usp.br>
2024-02-02 02:19:06 -0300

    add initial auth

---
+++ src/app/components/login/login-response.model.ts
@@ -0,0 +1,4 @@
+export interface LoginResponseInterface {
+    role: string[],
+    token: string
+}
---
+++ src/app/components/login/login.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginResponseInterface } from './login-response.model';
+import { environment } from 'src/environments/environment';
+import { LoginInterface } from './login.model';
+import { BehaviorSubject } from 'rxjs';
+import { Route, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  public roles: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
+
+  private readonly urlLogin: string = `${environment.api}/api/auth/login`;
+
+  constructor(private readonly http: HttpClient, private readonly router: Router) { }
+
+  public login(loginInterface: LoginInterface){
+    this.http.post<LoginResponseInterface>(this.urlLogin, loginInterface)
+      .subscribe(response => {
+          this.roles.next(response.role);
+          localStorage.setItem('Authorization', response.token);
+          this.router.navigate(['/form-list-obra']);
+      })
+  }
+
+}
