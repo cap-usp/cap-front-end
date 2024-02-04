32b6b45ccace599022306c22af66f462c1c62284
Gabriel Arrais <arrais.gbr@usp.br>
2024-02-03 22:54:40 -0300

    fixing duplicated path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,11 @@ import { FormListConstrutoraComponent } from './components/construtora/form-list
 import { FormListArquitetoComponent } from './components/arquiteto/form-list-arquiteto/form-list-arquiteto.component';
 import { FormListObraComponent } from './components/obra/form-list-obra/form-list-obra.component';
 import { FormListUsuarioComponent } from './components/usuario/form-list-usuario/form-list-usuario.component';
-import { ListObraComponent } from './components/obra/list-obra/list-obra.component';
 import { LoginComponent } from './components/login/login.component';
 import { ObraIndividualComponent } from './components/obra/obra-individual/obra-individual.component';
 
 const routes: Routes = [
   { path: '', component: FormListObraComponent, pathMatch: 'full'},
-  { path: '', component: PaginaInicialComponent, pathMatch: 'full'},
   { path: 'construtora', component: FormListConstrutoraComponent},
   { path: 'arquiteto', component: FormListArquitetoComponent},
   { path: 'obra', component: FormListObraComponent},
